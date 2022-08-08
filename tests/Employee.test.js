d60051a618805efcf9873c3bd2606cfc31debbb7
Peanut Butter <avni0000patel@gmail.com>
2022-08-07 21:31:28 -0400

    employee tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,32 @@
-const inquirer = requirer("inquirer");
+const Employee = require("../lib/Employee.js");
+console.log(Employee);
+
+describe("Employee", () => {
+    const employeeName = "Emmy";
+    const employeeId = "1";
+    const employeeEmail = "emmy@gmail.com";
+    const employeeRole = "Employee";
+
+    const employee = new Employee(employeeName, employeeId, employeeEmail);
+    console.log(employee);
+
+    it("Should return the employeeName", () => {
+        expect(employee.getName()).toEqual(employeeName);
+        console.log(employee.getName());
+    });
+
+    it("Should return the employeeId", () => {
+        expect(employee.getId()).toEqual(employeeId);
+        console.log(employee.getId());
+    });
+
+    it("Should return the employeeEmail", () => {
+        expect(employee.getEmail()).toEqual(employeeEmail);
+        console.log(employee.getEmail());
+    });
+
+    it("Should return the role", () => {
+        expect(employee.getRole()).toEqual(employeeRole);
+        console.log(employee.getRole());
+    });
+});
